b36ec5bcbc6364a8917f55fcb8d3f434e6db1386
Voldezhur <voldezhur@gmail.com>
2023-02-17 12:05:46 +0300

    #2 header written

---
+++ ПР2/#2/Circle.cpp
@@ -0,0 +1,43 @@
+// Реализация класса Circle
+
+
+#include "Circle.h"
+
+// Задать значения
+void Circle::set_circle(float r, float x, float y)
+{
+
+}
+
+// Площадь
+float Circle::area()
+{
+
+}
+
+/* 
+Можно ли описать окружность вокруг треугольника
+со сторонами a, b, c
+*/
+bool Circle::triangle_around(float a, float b, float c)
+{
+
+}
+
+/*
+Можно ли вписать в окружность треугольник
+со сторонами a, b, c
+*/   
+bool Circle::triangle_in(float a, float b, float c)
+{
+
+}
+
+/*
+Пересекается ли с данной окружностью 
+окружность с заданными аргументами
+*/
+bool Circle::check_circle(float r, float x_cntr, float y_cntr)
+{
+
+}
